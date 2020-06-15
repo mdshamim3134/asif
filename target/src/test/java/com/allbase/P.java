1991a4c010ffb7d58dbf25f96060d2129c8d3282
mdshamim3134 <45149075+mdshamim3134@users.noreply.github.com>
2020-06-15 10:21:22 -0400

    Update P.java
    adding veriable

--- target/src/test/java/com/allbase/P.java
+++ target/src/test/java/com/allbase/P.java
@@ -5,6 +5,7 @@ public class P {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int num=5;
+		int a=33;
 		for(int i=1;i<=10;i++) {
 			System.out.println( i+"x"+num+"="+" "+num*i);
 			
