cb90ae9b1a008dee44d261155ec8605a79598aae
asmatalukder3134 <asma.talukder@yahoo.com>
2020-06-15 10:14:54 -0400

    this is my p

---
+++ target/src/test/java/com/allbase/P.java
@@ -0,0 +1,15 @@
+package com.allbase;
+
+public class P {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=5;
+		for(int i=1;i<=10;i++) {
+			System.out.println( i+"x"+num+"="+" "+num*i);
+			
+		}
+
+	}
+
+}
